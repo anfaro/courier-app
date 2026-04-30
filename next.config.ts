14e081b75cb85422ef13299ae8480c4b1a55de67
anfaro <anang42429@gmail.com>
2026-05-01 00:14:09 +0700

    revert nextjs config back

--- next.config.ts
+++ next.config.ts
@@ -3,28 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['192.168.1.4'],
-  webpack: (config) => {
-    // Ensure watchOptions exists
-    if (!config.watchOptions) {
-      config.watchOptions = {};
-    }
-
-    config.watchOptions = {
-      ...config.watchOptions,
-      // Tell Webpack to explicitly ignore restricted Android system paths
-      // Make sure there are NO empty strings here!
-      ignored: [
-        '**/node_modules',
-        '**/.git',
-        '/data/data/**',
-        '/data/**',
-        '/storage/**',
-        '/' // Stops the watcher from trying to scan the absolute Android root
-      ],
-    };
-
-    return config;
-  },
 };
 
 export default nextConfig;
